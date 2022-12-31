0cc511b1515349ed571351a377e7307897245993
Elybourland <JEBourland@yahoo.com>
2022-12-31 15:08:25 -0800

    data fetching cleanup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReachDOM from 'react-dom/client';
 import App from './App';
-import searchImages from "./api";
-
-searchImages();
 
 const el = document.getElementById('root');
 const root = ReachDOM.createRoot(el);
